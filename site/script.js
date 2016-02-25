5e5af60405a885ef297e4a3283ca0a8ba6c6310e
oleksandrstarov <oleksandr.starov@gmail.com>
2016-02-25 23:09:56 +0200

    upload

--- site/script.js
+++ site/script.js
@@ -1,12 +1,12 @@
+
 console.log('started');
 var currentLine;
 var digitArray = [];
 var symbolsArray = ['mult', 'divide', 'plus', 'minus', 'equals'];
-var expression = [];
-var operations = [];
-var prevActionOperation = false;
 var argumentOne = null;
-var result = 0;
+var operationSymbol = null;
+var newOperation = null;
+
 window.onload = function(){
 	currentLine = document.getElementById('text');
 	for(var i=0; i<11; i++){
@@ -25,49 +25,52 @@ window.onload = function(){
 			operation(this.innerHTML);
 		}
 	}
-	console.log(digitArray);
 };
 
 function updateLine(value){
 	if(value == '.' && String(currentLine.innerHTML).indexOf(value) > 0) return null;
-	if(prevActionOperation) currentLine.innerHTML = 0;
-	if(currentLine.innerHTML == '0')currentLine.innerHTML = value;
-	else currentLine.innerHTML = currentLine.innerHTML + value;
-	prevActionOperation = false;
+	if(newOperation) currentLine.innerHTML = '';
+	newOperation = false;
+ 	currentLine.innerHTML = currentLine.innerHTML + value;
+
+	if(String(currentLine.innerHTML).indexOf('0') == 0 && String(currentLine.innerHTML).indexOf('.') != 1){
+		currentLine.innerHTML = String(currentLine.innerHTML).substring(1);
+	};
 };
 
 function operation(element){
-	if (!prevActionOperation) {
+	if(element == '=' && argumentOne != null && operationSymbol != null){
+		console.log('operationSymbol');
+		switch (operationSymbol) {
+			case '+':
+				argumentOne = Number(argumentOne) + Number(currentLine.innerHTML);
+				break;
+			case '-':
+				argumentOne = argumentOne - Number(currentLine.innerHTML);
+				break;
+			case 'x':
+				argumentOne = argumentOne * Number(currentLine.innerHTML);
+				break;
+			case '/':
+				argumentOne = argumentOne / Number(currentLine.innerHTML);
+				break;
+			case 'x':
+				argumentOne = argumentOne * Number(currentLine.innerHTML);
+				break;
 
-			switch (element) {
-				case '+':
-					argumentOne = argumentOne + Number(currentLine.innerHTML);
-					break;
-				case '-':
-					argumentOne = argumentOne - Number(currentLine.innerHTML);
-					break;
-				case 'x':
-					argumentOne = argumentOne * Number(currentLine.innerHTML);
-					break;
-				case '/':
-					argumentOne = argumentOne / Number(currentLine.innerHTML);
-					break;
-				case 'x':
-					argumentOne = argumentOne * Number(currentLine.innerHTML);
-					break;
-				default:
-					argumentOne = argumentOne;
+			default:
+				break;
 		}
-
-			currentLine.innerHTML = argumentOne;
-
-
-
-
-		prevActionOperation = true;
-
-
+		currentLine.innerHTML = argumentOne;
+		argumentOne = null;
+		operationSymbol = null;
+		newOperation = false;
 	}
-
+	else{
+		console.log(element);
+		operationSymbol = element;
+		argumentOne = currentLine.innerHTML;
+		newOperation = true;
+	}
 
 };
